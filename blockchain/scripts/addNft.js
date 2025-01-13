66a4d03d9e02f3f329b7c9b62934281decef6902
in-th3-l00p <indianucode0303@gmail.com>
2025-01-13 13:00:22 +0200

    added cors

--- blockchain/scripts/addNft.js
+++ blockchain/scripts/addNft.js
@@ -5,11 +5,11 @@ const { contractAddress, contractAbi } = require("./contract");
 async function main() {
     const contract = new ethers.Contract(contractAddress, contractAbi, ethers.provider);
     const [deployer] = await ethers.getSigners();
-    const tx = await contract.connect(deployer).createBadge({
-        "_name": "iosub",
-        "_description": "iosub",
-        "_metadataURI": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.izismile.com%2Fimg%2Fimg6%2F20130926%2F640%2Fthe_internet_is_a_weird_and_unusual_place_640_06.jpg&f=1&nofb=1&ipt=5a4ad551fce33b31eba3c38cd5e39786f7d561476c485945e7298a19a56fedc6&ipo=images"
-    });
+    const tx = await contract.connect(deployer).createBadge(
+        "Level 3 - bec",
+        "Ai aruncat 1000 de beci la colectare!",
+        "https://xfckpxetjwqzqrpknjsq.supabase.co/storage/v1/object/public/NFTS/G2.svg"
+    );
 
     await tx.wait();
     console.log(`Transaction successful! Hash: ${tx.hash}`);
